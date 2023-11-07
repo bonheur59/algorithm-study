65ffb124f4f2fe9142a4016fad6f369da822258e
Yoonhee Lee <105180536+yoonhee032@users.noreply.github.com>
2023-11-07 10:48:47 +0900

    [level unrated] Title: 점의 위치 구하기, Time: 0.01 ms, Memory: 72.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120841. 점의 위치 구하기/점의 위치 구하기.java
+++ 프로그래머스/unrated/120841. 점의 위치 구하기/점의 위치 구하기.java
@@ -1,21 +1,26 @@
 class Solution {
     public int solution(int[] dot) {
     
-    int answer = 0;
+        int answer = 0;
+        int x = dot[0];
+        int y = dot[1];
         
-    int x = dot[0] > 0 ? 1 : 0;
-    int y = dot[1] > 0 ? 1 : 0;
+        if(x > 0) {
+           if(y > 0) {
+               answer = 1;
+           } else {
+               answer = 4;
+           }
+        } else 
+             if(y > 0) {
+               answer = 2;
+           } else {
+               answer = 3;
+           }
         
-    if(x+y == 2){
-        return 1;
-    } else if(x+y == 0){
-        return 3;
-    } else{
-        if (x<y){
-            return 2; 
-        } else{
-            return 4;
-        }
-    }
+        return answer;
     }
 }
+
+
+      
