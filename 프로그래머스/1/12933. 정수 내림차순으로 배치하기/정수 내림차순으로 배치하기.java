e63ade979125eea7d87dfa03401c23a012cac3e8
Yoonhee Lee <105180536+yoonhee032@users.noreply.github.com>
2023-11-09 14:53:35 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 0.52 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.java
+++ 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.java
@@ -2,21 +2,12 @@ import java.util.*;
 
 class Solution {
     public long solution(long n) {
-        long answer = 0;
-        
-        String[] numArray = Long.toString(n).split("");
-        
+        String[] numArray = String.valueOf(n).split("");      
         Arrays.sort(numArray);
         
         StringBuilder sb = new StringBuilder();
-        
         for (String s : numArray) sb.append(s);
         
-        answer = Long.parseLong(sb.reverse().toString());
-        
-        return answer;
-        
-        
-        
+        return  Long.parseLong(sb.reverse().toString());
     }
 }
